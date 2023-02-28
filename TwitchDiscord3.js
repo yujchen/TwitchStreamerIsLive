f592385d547d654e915af4c32b16cf753a2b5883
Yujui Chen <yujuich@gmail.com>
2023-03-01 00:38:56 +0800

    added core files for the functionality

---
+++ TwitchDiscord3.js
@@ -0,0 +1,82 @@
+let streamID = process.argv[2];
+const UsherService = require('./usher3.js');
+const {Intents, Client} = require('discord.js');
+const dstoken = require('dotenv');
+dstoken.config();
+let dsChannelID = process.env.YSTWITCHEVENT;
+
+const dsclient = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+
+dsclient.on('ready',()=>{
+    console.log("DSBot is ready");
+    const channel = dsclient.channels.cache.get(dsChannelID);
+    // channel.send(`${streamID}+bark1`);
+    // channel.send(`${streamID}+bark2`);
+    // channel.send(`${streamID}+bark3`);
+    // channel.send(`${streamID}+bark4`);
+    // channel.send(`${streamID}+bark5`);
+    // channel.send(`${streamID}+bark6`);
+    TwitchStatus(streamID);
+});
+
+
+
+dsclient.login(process.env.TOKEN);
+
+async function TwitchStatus(streamID){
+    let y = -1;
+    let count = 0;
+    if (streamID === null || streamID === undefined){
+        console.log("Error: no stream name input");
+        process.exit(1);
+    }
+    while(true){
+        let status = await UsherService.isChannelLive(streamID);
+        console.log(`${getCurrentTime()} y is ${y}, Usher is ${status}`);
+        if (y !== 1 && status === 1){
+            if (status === 1){
+                const channel = dsclient.channels.cache.get(dsChannelID);
+                channel.send(`${streamID} is online.`);
+                console.log(`${getCurrentTime()} ${streamID} is online.`);
+                y = 1;
+            }else{
+                console.log(`Check failed. y is ${y} status ${status}`);
+            }
+        }
+        else if (y !== 0 && (status === 0 || status === 404)){
+            if (status === 0 || status === 404){
+                const channel = dsclient.channels.cache.get(dsChannelID);
+                channel.send(`${streamID} is offline.`);
+                console.log(`${getCurrentTime()} ${streamID} is offline.`);
+                y = 0;  
+            }else{
+                console.log(`Check failed. y is ${y} status ${status}`);
+            }            
+        }
+        else if (y !== 2 && status === 2){
+            if (status === 2){
+                const channel = dsclient.channels.cache.get(dsChannelID);
+                channel.send(`${streamID} does not exist.`);
+                console.log(`${getCurrentTime()} ${streamID} does not exist.`);
+                y = 2;
+            }else{
+                console.log(`Check failed. y is ${y} status ${status}`);
+            }
+        }
+        await new Promise(resolve => setTimeout(resolve, 3000));
+    }
+}
+
+function getCurrentTime(){
+	let today = new Date(); 
+	let dd = String(today.getDate()).padStart(2, '0');
+	let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	let yyyy = today.getFullYear();
+	let time = (today.getHours() < 10 ? '0'+today.getHours() : String(today.getHours())) + (today.getMinutes() < 10 ? '0'+today.getMinutes() : String(today.getMinutes()))  + (today.getSeconds() < 10 ? '0'+today.getSeconds() : String(today.getSeconds()));
+	return '['+yyyy + '-' + mm + '-' + dd + '-' + time+']';
+}
---
+++ usher3.js
@@ -0,0 +1,107 @@
+const { response } = require('express');
+const https = require('https');
+
+async function isChannelLive(channelName) {
+  const clientID = 'kimne78kx3ncx6brgo4mv6wki5h1ko';
+
+  const gqlQuery = {
+    operationName: 'PlaybackAccessToken',
+    query:
+      'query PlaybackAccessToken($login: String!, $isLive: Boolean!, $playerType: String!) { streamPlaybackAccessToken(channelName: $login, params: {platform: \"web\", playerBackend: \"mediaplayer\", playerType: $playerType}) @include(if: $isLive) { value signature __typename } }',
+    variables: { isLive: true, login: channelName, playerType: 'site' },
+  };
+
+  try {
+    //From TwitchGQL we get the token, this token validates the existence of the user, which then can be used to redeemed at usherttv for livestream files if the streamer is live.
+    let tokenResponse = await sendRequest('gql.twitch.tv','POST', '/gql', gqlQuery, clientID); 
+    if (tokenResponse.statusCode === 404){
+        //console.log("it is 404");
+        return 404;
+    }
+    //console.log(tokenResponse);
+    tokenResponse = JSON.parse(tokenResponse);
+    if (!tokenResponse.data || !tokenResponse.data.streamPlaybackAccessToken) {
+        return 2; //Error getting response
+    }
+    const { value: token, signature } = tokenResponse.data.streamPlaybackAccessToken;
+    const streamResponse = await sendRequest(
+        'usher.ttvnw.net',
+        'GET',
+        `/api/channel/hls/${channelName}.m3u8?player=twitchweb&p=123456&type=any&allow_source=true&allow_audio_only=true&allow_spectre=false&sig=${signature}&token=${encodeURIComponent(token)}&fast_bread=True`,
+        null,
+        null
+    );
+    //console.log(streamResponse);
+    if (streamResponse === 404){
+        console.log(channelName +" offline");
+        return 404;
+    }
+    //console.log(streamResponse);
+    if (streamResponse.includes('transcode_does_not_exist')) {
+        return 0;
+    } else if (streamResponse.includes('#EXTM3U')) {
+        console.log(channelName+ " online!");
+        return 1;
+    } else {
+        return 0;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
+
+function sendRequest(host,method, path, data, clientID) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: host,
+      path,
+      method,
+      headers: {
+        'Client-ID': clientID,
+      },
+    };
+    //console.log(host+path);
+    const req = https.request(options, (res) => {
+      let responseBody = '';
+
+      res.on('data', (chunk) => {
+        responseBody += chunk;
+      });
+
+      res.on('end', () => {
+        if (res.statusCode === 404){
+            resolve(res.statusCode);
+        }
+        resolve(responseBody);
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    if (data) {
+      const body = JSON.stringify(data);
+      req.write(body);
+    }
+
+    req.end();
+  });
+}
+
+async function ICLmain(CN) {
+    try {
+      //const channelName = CN;
+      const liveStatus = await isChannelLive(CN);
+      return liveStatus;
+      //console.log(`Live status of ${channelName}: ${liveStatus}`);
+    } catch (error) {
+      console.error(error);
+    }
+}
+
+let CN = process.argv[2];
+ICLmain(CN);
+module.exports.ICLmain = ICLmain;
+module.exports.isChannelLive = isChannelLive;
