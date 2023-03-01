07a8e6191087798cd09d7c1bfdf436b9288ee860
Yujui Chen <yujuich@gmail.com>
2023-03-01 21:54:31 +0800

    Tweak some stability issues

--- TwitchDiscord3.js
+++ TwitchDiscord3.js
@@ -2,6 +2,7 @@ let streamID = process.argv[2];
 const UsherService = require('./usher3.js');
 const {Intents, Client} = require('discord.js');
 const dstoken = require('dotenv');
+const RETRY_CHECKS = 3;
 dstoken.config();
 let dsChannelID = process.env.YSTWITCHEVENT;
 
@@ -40,23 +41,49 @@ async function TwitchStatus(streamID){
         console.log(`${getCurrentTime()} y is ${y}, Usher is ${status}`);
         if (status !== 2){
             if (y !== 1 && status === 1){
-                if (status === 1){
+                let defCounter = 0;
+                while(defCounter !== RETRY_CHECKS){
+                    let i = await UsherService.isChannelLive(streamID);
+                    if (i === status){
+                        console.log("checking for switching online. score:" + defCounter);
+                        defCounter++;
+                    }
+                    else{
+                        console.log(`Onside. defCounter check failed.`);
+                        break;
+                    }
+                    //await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+                if (defCounter === RETRY_CHECKS){
                     const channel = dsclient.channels.cache.get(dsChannelID);
-                    channel.send(`${streamID} is online.`);
+                    channel.send(`${streamID} is online. Come watch! https://twitch.tv/${streamID} !`);
                     console.log(`${getCurrentTime()} ${streamID} is online. Status is ${status}`);
                     y = 1;
                 }else{
-                    console.log(`Check failed. y is ${y} status ${status}`);
+                    console.log(`Onside.Check failed. defCounter is ${defCounter} status ${status}`);
                 }
             }
             else if (y !== 0 && (status === 0 || status === 404)){
-                if (status === 0 || status === 404){
+                let defCounter = 0;
+                while(defCounter !== RETRY_CHECKS){
+                    let i = await UsherService.isChannelLive(streamID);
+                    if (i === status){
+                        console.log("checking for switching to offline. score:" + defCounter);
+                        defCounter++;
+                    }
+                    else{
+                        console.log(`Offside. defCounter check failed.`);
+                        break;
+                    }
+                    //await new Promise(resolve => setTimeout(resolve, 1000));
+                }
+                if (defCounter === RETRY_CHECKS){
                     const channel = dsclient.channels.cache.get(dsChannelID);
                     channel.send(`${streamID} is offline.`);
                     console.log(`${getCurrentTime()} ${streamID} is offline. Status is ${status}`);
                     y = 0;  
                 }else{
-                    console.log(`Check failed. y is ${y} status ${status}`);
+                    console.log(`Offside. Check failed. defCounter is ${defCounter} status ${status}`);
                 }            
             }
         }
@@ -64,7 +91,7 @@ async function TwitchStatus(streamID){
             console.log("Error 2 received");
             console.log(`${getCurrentTime()}, Usher is ${status}`);
         }
-        await new Promise(resolve => setTimeout(resolve, 3000));
+//        await new Promise(resolve => setTimeout(resolve, 3000));
     }
 }
 
